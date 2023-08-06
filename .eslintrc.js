2ff9ded0893473b323a576f7c1a9abfe0eb2619c
Kathiravan K <kathirr007@gmail.com>
2023-08-07 02:22:25 +0530

    basic-webpack-project base completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,9 @@ process.env.ESLINT_TSCONFIG = 'tsconfig.json'
 
 module.exports = {
   extends: '@antfu',
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'unused-imports/no-unused-imports': 0,
+  },
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,39 @@
 import path from 'node:path'
 import type webpack from 'webpack'
-import type { PathData } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+import CopyPlugin from 'copy-webpack-plugin'
 
 // in case you run into any typescript error when configuring `devServer`
 import 'webpack-dev-server'
+
+// const CopyPlugin = require('copy-webpack-plugin')
 
 const config: webpack.Configuration = {
   mode: 'production',
   entry: {
     index: './src/index.js',
-    product: './src/products.js',
+    courses: './src/pages/courses.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // assetModuleFilename: 'images/[hash][ext]',
+    // assetModuleFilename: (pathData: PathData) /** function to keep the original file name */ => {
+    //   const filepath = path
+    //     .dirname(pathData.filename as string)
+    //     .split('/')
+    //     .slice(1)
+    //     .join('/')
+    //   return `${filepath}/[name].[hash][ext][query]`
+    // },
+    clean: true,
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
-    port: 3100,
+    // port: 3100,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      chunks: ['index'],
-      inject: true,
-      filename: 'index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/products.html'),
-      chunks: ['product'],
-      inject: true,
-      filename: 'products.html',
-    }),
-  ],
   module: {
     rules: [
       {
@@ -42,7 +45,7 @@ const config: webpack.Configuration = {
         test: /\.(css)$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'css-loader', options: { } },
         ],
         // use: ['style-loader', 'css-loader'], // loaders executed from right to left
       },
@@ -50,12 +53,12 @@ const config: webpack.Configuration = {
         test: /\.s[ac]ss$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'css-loader', options: { } },
           { loader: 'sass-loader' },
         ],
       },
       {
-        test: /\.(png|jpg|gif|svg|webp)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         type: 'asset/resource',
       },
       {
@@ -67,20 +70,32 @@ const config: webpack.Configuration = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    // assetModuleFilename: 'images/[hash][ext]',
-    assetModuleFilename: (pathData: PathData) /** function to keep the original file name */ => {
-      const filepath = path
-        .dirname(pathData.filename as string)
-        .split('/')
-        .slice(1)
-        .join('/')
-      return `${filepath}/[name].[hash][ext][query]`
-    },
-    clean: true,
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      // template: './src/index.html',
+      chunks: ['index'],
+      // inject: true,
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/pages/courses.html'),
+      // template: './src/pages/courses.html',
+      chunks: ['courses'],
+      // inject: true,
+      filename: 'courses.html',
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/assets/images/'),
+        // to: path.resolve(__dirname, 'dist'),
+        to({ context, absoluteFilename }) {
+          return `assets/images/${path.relative(context, absoluteFilename as string)}`
+        },
+      }],
+    }),
+  ],
+
 }
 
 export default config
